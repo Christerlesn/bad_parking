943d4433b55a3cc4011b828187e11407640931ec
Christerle_Nounoute <christerlesn@gmail.com>
2021-03-15 11:35:44 -0700

    trying to fix photo display issue

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,48 +1,55 @@
 class PhotosController < ApplicationController
 
-    get "/photos" do
-        @photos = Photo.all
-        @users = User.all
-        erb :"photos/photo_index"
+  get '/photos' do
+      @photos = Photo.all
+      @users = User.all
+      erb :'photos/photo_index'
+    end
+  
+    get '/photos/new' do
+      erb :"photos/photo_new"
+    end
+  
+    get '/photos/:id' do
+      @photo = Photo.find_by(params[:id])
+      erb :"photos/photo"
+    end
+  
+    get '/photos/:id/edit' do
+      @photo = Photo.find(params[:id])
+      erb :"photos/photo_edit"
+    end
+
+    post '/photos' do
+      puts "===================================="
+      # puts params.inspect
+      puts "===================================="
+      @filename = params[:file][:filename]
+      file = params[:file][:tempfile]
+      @photo= Photo.create!(url:@filename)
+      current_user.photos << @photo
+
+      File.open("./public/images/#{@photo.url}", 'wb') do |f|
+        f.write(file.read)
       end
+      
+      redirect to "/photos/#{@photo.id}"
+    end
     
-      get "/photos/new" do
-        erb :"photos/photo_new"
-      end
-    
-      get "/photos/:id" do
-        @photo = Photo.find_by(params[:id])
-        erb :"photos/photo"
-      end
-    
-      get "/photos/:id/edit" do
-        @photo = Photo.find_by(params[:id])
-        erb :"photos/photo_edit"
-      end
-    
-      post "/photos" do
-        @filename = params[:file][:filename]
-        file = params[:file][:tempfile]
-        @photo= Photo.create(url: @filename)
-        current_user.photos << @photo
-    
-        File.open("./public/images/#{@photo.url}", 'wb') do |f|
-          f.write(file.read)
-        end
-        redirect to "/photos/#{@photo.id}"
-      end
-    
-      post "/photos/:id" do
-        @photo = Photo.find_by(params[:id])
-        @photo.caption = params['photo']['caption']
-        redirect to "/photos/#{@photo.id}"
-      end
-    
-    
-    
-      # delete "/photo/:id/delete" do
-      #   #redirect to "/photo/index"
-      # end
+  
+    post '/photos/:id' do
+      @photo = Photo.find(params[:id])
+      @photo.name = params['photo']['name']
+      @photo.save
+      redirect to "/photos/#{@photo.id}"
+    end
+
+    delete "/photos/:id/delete" do
+      @photo = Photo.find_by_id(params[:id])
+      @photo.delete
+      
+      redirect to "/photo/index"
+    end
 
 
 
