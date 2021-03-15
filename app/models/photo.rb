469b9f3999fa616a6d3da6cf10080c614eb34dab
Christerle_Nounoute <christerlesn@gmail.com>
2021-03-14 21:57:37 -0700

    created Images Uploader class

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
+require_relative '../uploader/upload_photo'
+
 class Photo < ActiveRecord::Base
     belongs_to :user
+    validates :url, presence: true, uniqueness:true
+    mount_uploader :photo, ImagesUploader
 end
