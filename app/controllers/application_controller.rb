213146ebb675c264879dca65cfd141baf83ffedf
Christerle_Nounoute <christerlesn@gmail.com>
2021-03-14 19:46:52 -0700

    created User database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,10 +65,9 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user || = User.find_by(id: session[:user_id]) if session[:user_id]
-      end
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
-    
+
   end
 
-end #End of Application Controller
+end # End of Application Controller.rb
