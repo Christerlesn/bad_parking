977aeb7027b16f090009bf361a60952f19817b9d
Christerle_Nounoute <christerlesn@gmail.com>
2021-03-14 18:45:36 -0700

    Created routes in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,45 @@ class ApplicationController < Sinatra::Base
     erb :"users/index"
   end
 
+  get "/signup" do
+    erb :"users/sign_up"
+        # POST to "/photo/index"
+  end
+
+  get "/login" do
+    erb :"users/login"
+    # POST to "/photo/index"
+  end
+
+  get "/photo/index" do
+    #Shows all the photos
+    erb :"photos/photo_index"
+  end
+
+  post "/photo/index" do
+    #Shows all the photos
+    erb :"photos/photo_index"
+  end
+
+  get "/photo/new" do
+    erb :"photos/photo_new"
+    #POST to "/photo/index"
+  end
+
+  get "/photo/:id" do
+    erb :"photos/photo"
+  end
+
+  get "/photo/:id/edit" do
+    erb :"photos/photo_edit"
+  end
+
+  patch "/photo/:id/edit" do
+    #POST to "/photo/:id"
+  end
+
+  delete "/photo/:id/delete" do
+    #redirect to "/photo/index"
+  end
+
 end
