8bb693021ea687579d865551f5d9bccf89cfe4d9
Christerle Nounoute <christerlesn@gmail.com>
2021-03-21 20:09:44 +0000

    deleted lib

--- config/environment.rb
+++ config/environment.rb
@@ -18,5 +18,5 @@ CarrierWave.configure do |config|
   config.root = File.dirname(__FILE__) + "/public"
 end
 
-# require './app/controllers/application_controller'
-require_all 'app'
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
